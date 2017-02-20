d0c80f9c5ad42ef774900dfd285b64f3d555057e
sina <sina6002@gmail.com>
2017-02-20 14:13:49 +0000

    Initial commit with all that's necessary

---
+++ src/main/java/ferromone/trails/trainsim/App.java
@@ -0,0 +1,17 @@
+package ferromone.trails.trainsim;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+  static final Logger logger = LoggerFactory.getLogger(App.class);
+
+  public static void main(String[] args) {
+    logger.warn("hello world");
+
+  }
+}
---
+++ src/test/java/ferromone/trails/trainsim/AppTest.java
@@ -0,0 +1,23 @@
+package ferromone.trails.trainsim;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+
+  /**
+   * Rigourous Test :-)
+   */
+  @Test
+  public void testApp() {
+    assertTrue(true);
+  }
+}
